71531cede27906bc7ba442ea8f1976a8aa0c73c3
yezi <allthetimesh29@gmail.com>
2023-11-20 18:41:07 +0900

    IOS_Swift_App_11_Commit_2023.11.20 1. ObserableObject - @Published,@ObservedObject 2. @State
    by.allthetimesh29

--- Demo/Diff.swift
+++ Demo/Diff.swift
@@ -9,13 +9,17 @@ import SwiftUI
 
 struct Diff: View {
     let myCar = Car(name: "스턱카", owner: "스턱")
-  @ObservedObject  var myKar = Kar(name: "클랫카", owner: "클래스")
-    
+  @ObservedObject  var myKar = Kar(name: "클랫카", owner: "클래스") // @ObservedObject는 Class, 값의 변경을 감지하고, 변경 해주는 역할
+    @State var name: String = "" //@State는 Struct
+   
     
     var body: some View {
         VStack{
             Text("\(myCar.name)의 주인은 \(myCar.owner)입니다.")
             Text("\(myKar.name)의 주인은 \(myKar.owner)입니다.")
+            
+            TextField("입력", text: $name).multilineTextAlignment(.center)
+            
             Button{
                 var broCar = myKar
                 
@@ -45,8 +49,8 @@ struct Car{
 - error:  Class 'Kar' has no initializers
  그러나, 상단 class 코드와 같이 class는 메소드가 필수임
  **/
-class Kar: ObservableObject {
-  @Published  var name: String
+class Kar: ObservableObject { // 관찰이 가능한 객체임을 선언, Class에서 사용
+  @Published  var name: String // 바뀌는 값에 대한 공지
     var owner: String
     
     func syaHi() {
