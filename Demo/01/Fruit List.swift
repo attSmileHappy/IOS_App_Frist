b5d67751f27702d93330934b76f52844841d3c01
yezi <allthetimesh29@gmail.com>
2024-02-28 07:46:33 +0900

    IOS_Swift_App_2.6_Commit_2024.02.28 1. Data modeling
    by.allthetimesh29

--- Demo/01/Fruit List.swift
+++ Demo/01/Fruit List.swift
@@ -8,20 +8,34 @@
 import SwiftUI
 
 struct Fruit_List: View {
-
-    var fruits = ["Apple", "Banana", "Cherry", "Orange", "Strawberry", "Pineapple", "Watermelon", "Grape", "Mango", "Kiwi", "Peach", "Plum", "Apricot", "Pear", "Lemon", "Lime", "Blueberry", "Raspberry", "Blackberry", "Cranberry", "Coconut", "Fig", "Guava", "Papaya", "Passionfruit", "Pomegranate", "Tangerine", "Lychee", "Dragonfruit"]
-
-    var fruitPrices: [Int] = [100, 200, 300, 150, 250, 180, 500, 450, 700, 300, 120, 220, 400, 380, 600, 800, 350, 270, 320, 420, 900, 550, 680, 730, 850, 950, 890, 770, 630, 740]
+    
+    var fruits = [
+        Fruit(imageName: "applelogo", name: "Apple", color: .red, price: 1000),
+        Fruit(imageName: "b.circle.fill", name: "Banana", color: .yellow, price: 1500),
+        Fruit(imageName: "c.circle.fill", name: "Cherry", color: .pink, price: 2000),
+        Fruit(imageName: "k.circle.fill", name: "Kiwi", color: .green, price: 3000)
+    ]
 
     var body: some View {
         NavigationStack{
             List(0..<fruits.count, id: \.self) { index in
-       
-                    Text("\(fruits[index]) \(fruitPrices[index])원")
+                HStack{
+                    Image(systemName: fruits[index].imageName)
+                        .foregroundColor(fruits[index].color)
+                    Text("\(fruits[index].name)")
+                    Text("\(fruits[index].price)원")
+                }
             }
             .navigationTitle("Fruit List")
         }
     }
+}
+
+struct Fruit {
+    let imageName: String
+    let name: String
+    let color: Color
+    let price: Int
 }
 
 #Preview {
