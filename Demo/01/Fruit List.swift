9401512669bdca2beb2fd08e76ba5e715a06f823
yezi <allthetimesh29@gmail.com>
2024-02-28 08:20:25 +0900

    IOS_Swift_App_2.7_Commit_2024.02.28 1. List Append, List Remove 2. .onDelete 3. List UUID
    by.allthetimesh29

--- Demo/01/Fruit List.swift
+++ Demo/01/Fruit List.swift
@@ -9,34 +9,61 @@ import SwiftUI
 
 struct Fruit_List: View {
     
+   @State 
     var fruits = [
         Fruit(imageName: "applelogo", name: "Apple", color: .red, price: 1000),
         Fruit(imageName: "b.circle.fill", name: "Banana", color: .yellow, price: 1500),
         Fruit(imageName: "c.circle.fill", name: "Cherry", color: .pink, price: 2000),
         Fruit(imageName: "k.circle.fill", name: "Kiwi", color: .green, price: 3000)
     ]
-
+    
+    @State var fruitName: String = ""
+    
     var body: some View {
         NavigationStack{
-            List(0..<fruits.count, id: \.self) { index in
+            VStack{
                 HStack{
-                    Image(systemName: fruits[index].imageName)
-                        .foregroundColor(fruits[index].color)
-                    Text("\(fruits[index].name)")
-                    Text("\(fruits[index].price)원")
+                    TextField("추가할 과일:", text: $fruitName)
+                        .padding()
+                    Button{
+                        fruits.append(Fruit(imageName: "applelogo", name: fruitName, color: .black, price: 1000))
+                    } label: {
+                        Text("추가")
+                            .padding()
+                            .background(.blue)
+                            .foregroundColor(.white)
+                            .cornerRadius(10)
+                    }
                 }
+                .padding()
+                
+                List {
+                    ForEach(fruits, id: \.self) { fruit in
+                        HStack {
+                            Image(systemName: fruit.imageName)
+                                                   .foregroundColor(fruit.color)
+                            Text("\(fruit.name)")
+                            Text("\(fruit.price)원")
+                        }
+                    }
+                    .onDelete { offsets in
+                        fruits.remove(atOffsets: offsets)
+                    }
+                }
+                .navigationTitle("Fruit List")
             }
-            .navigationTitle("Fruit List")
         }
     }
 }
 
-struct Fruit {
+struct Fruit: Identifiable, Hashable {
+    let id = UUID()
     let imageName: String
     let name: String
     let color: Color
     let price: Int
 }
+
 
 #Preview {
     Fruit_List()
